f5b1a270fdf65bd8fc251ef1f426de57ade19a80
xDysar <lucasbnicolas@gmail.com>
2022-10-28 17:35:54 -0300

    Se agrego alerta al formulario

--- js/app.js
+++ js/app.js
@@ -2,12 +2,33 @@ document.addEventListener('DOMContentLoaded', function () {
   const inputEmail = document.querySelector('#email');
   const inputAsunto = document.querySelector('#asunto');
   const inputMensaje = document.querySelector("#mensaje");
+  const formulario = document.querySelector('#formulario');
 
   inputEmail.addEventListener('blur', validar);
   inputAsunto.addEventListener("blur", validar);
   inputMensaje.addEventListener("blur", validar);
 
   function validar (e) {
-    console.log(e.target.value)
+    if (e.target.value.trim() === "") {
+      mostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement);
+      return;
+    }
+    limpiarAlerta(e.target.parentElement);
+  }
+
+  function mostrarAlerta(mensaje, referencia) {
+    limpiarAlerta(referencia);
+
+    const error = document.createElement('P');
+    error.textContent = mensaje;
+    error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center', 'error')
+    referencia.appendChild(error)
+  }
+
+  function limpiarAlerta(referencia) {
+    const alerta = referencia.querySelector(".error");
+    if (alerta) {
+      alerta.remove();
+    }
   }
 });
